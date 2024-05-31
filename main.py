286e4cdc7db9b926fa1aa481954e0a35bc580aa5
Wouter Gritter <wouter@gritter.me>
2024-05-31 17:13:03 +0200

    Refactor fetchers to minimize rs485 reads and add computed amperage topics

--- main.py
+++ main.py
@@ -8,28 +8,41 @@ import paho.mqtt.client as mqtt
 
 
 class Fetcher(ABC):
+
     @abstractmethod
-    def fetch(self, rs485: minimalmodbus.Instrument) -> float:
+    def fetch(self, previous_values: dict[str, float]) -> Optional[float]:
         pass
 
 
-class FloatFetcher(Fetcher):
-    def __init__(self, address: int):
-        self.address = address
+class ReaderFetcher(Fetcher):
 
-    def fetch(self, rs485: minimalmodbus.Instrument) -> float:
-        return rs485.read_float(self.address)
+    def __init__(self, address: int, modifier: Optional[Callable[[float], float]] = None):
+        self.address = address
+        self.modifier = modifier
+
+    def fetch(self, previous_values: dict[str, float]) -> Optional[float]:
+        try:
+            value = rs485.read_float(self.address)
+            if self.modifier is not None:
+                value = self.modifier(value)
+
+            return value
+        except Exception as ex:
+            print(f'Error reading address {self.address:X}.')
+            print(ex)
+            return None
 
 
 class ComputedFetcher(Fetcher):
-    def __init__(self, fetchers: list[Fetcher], expression: Callable[[list[float]], float]):
-        self.fetchers = fetchers
+
+    def __init__(self, in_attributes: list[str], expression: Callable[[list[float]], float]):
+        self.in_attributes = in_attributes
         self.expression = expression
 
-    def fetch(self, rs485: minimalmodbus.Instrument) -> float:
-        values = []
-        for fetcher in self.fetchers:
-            values.append(fetcher.fetch(rs485))
+    def fetch(self, previous_values: dict[str, float]) -> Optional[float]:
+        values = [previous_values[attribute] for attribute in self.in_attributes]
+        if None in values:
+            return None
 
         return self.expression(values)
 
@@ -42,43 +55,41 @@ MQTT_TOPIC_FORMAT = os.getenv('MQTT_TOPIC_PREFIX', 'dts353f/{attribute}')
 MQTT_QOS = int(os.getenv('MQTT_QOS', '1'))
 MQTT_RETAIN = os.getenv('MQTT_RETAIN', 'true') == 'true'
 
-ATTRIBUTE_MAP = {
-    'energy/delivery': FloatFetcher(0x0108),
-    'energy/redelivery': FloatFetcher(0x0110),
-    'energy/total': ComputedFetcher([FloatFetcher(0x0108), FloatFetcher(0x0110)], lambda values: values[0] - values[1]),
-    'power/total': FloatFetcher(0x001C),
-    'power/l1': FloatFetcher(0x001E),
-    'power/l2': FloatFetcher(0x0020),
-    'power/l3': FloatFetcher(0x0022),
-    'voltage/l1': FloatFetcher(0x000E),
-    'voltage/l2': FloatFetcher(0x0010),
-    'voltage/l3': FloatFetcher(0x0012),
-    'voltage/average': ComputedFetcher([FloatFetcher(0x000E), FloatFetcher(0x0010), FloatFetcher(0x0012)], lambda values: sum(values) / len(values)),
+ATTRIBUTE_FETCHERS = {
+    'energy/delivery': ReaderFetcher(0x0108),
+    'energy/redelivery': ReaderFetcher(0x0110),
+    'energy/total': ComputedFetcher(['energy/delivery', 'energy/redelivery'], lambda v: v[0] - v[1]),
+    'power/total': ReaderFetcher(0x001C, lambda p: p * 1000),
+    'power/l1': ReaderFetcher(0x001E, lambda p: p * 1000),
+    'power/l2': ReaderFetcher(0x0020, lambda p: p * 1000),
+    'power/l3': ReaderFetcher(0x0022, lambda p: p * 1000),
+    'voltage/l1': ReaderFetcher(0x000E),
+    'voltage/l2': ReaderFetcher(0x0010),
+    'voltage/l3': ReaderFetcher(0x0012),
+    'voltage/average': ComputedFetcher(['voltage/l1', 'voltage/l2', 'voltage/l3'], lambda v: sum(v) / len(v)),
+    'amperage/l1': ComputedFetcher(['power/l1', 'voltage/l1'], lambda v: v[0] / v[1]),
+    'amperage/l2': ComputedFetcher(['power/l2', 'voltage/l2'], lambda v: v[0] / v[1]),
+    'amperage/l3': ComputedFetcher(['power/l3', 'voltage/l3'], lambda v: v[0] / v[1]),
+    'amperage/total': ComputedFetcher(['amperage/l1', 'amperage/l2', 'amperage/l3'], lambda v: sum(v)),
 }
+
 
 mqttc: Optional[mqtt.Client] = None
 rs485: Optional[minimalmodbus.Instrument] = None
 
 
-def update_attribute(attribute: str):
-    fetcher = ATTRIBUTE_MAP[attribute]
-
-    try:
-        result = fetcher.fetch(rs485)
-    except Exception as ex:
-        print(f'Error reading attribute {attribute}.')
-        print(ex)
-        return
-
-    result = str(round(result, 3))
-
+def publish_attribute(attribute: str, value: float):
     topic = MQTT_TOPIC_FORMAT.replace('{attribute}', attribute)
-    mqttc.publish(topic, result, qos=MQTT_QOS, retain=MQTT_RETAIN)
+    mqttc.publish(topic, str(round(value, 2)), qos=MQTT_QOS, retain=MQTT_RETAIN)
 
 
 def update_attributes():
-    for attribute in ATTRIBUTE_MAP:
-        update_attribute(attribute)
+    values = {}
+    for attribute, fetcher in ATTRIBUTE_FETCHERS.items():
+        value = fetcher.fetch(values)
+        values[attribute] = value
+        if value is not None:
+            publish_attribute(attribute, value)
 
 
 def main():
